3e14a0267720976c96174a87d48a04f80d7c9a6c
achapur <achapur@palaceresorts.com>
2022-07-27 16:35:29 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import foodsData from './foods.json';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import {useState} from 'react'
+import FoodBox from './components/foodbox';
+import AddFoodForm from './components/addfoodform';
+import Search from './components/search';
 
 function App() {
+  const [foods, setFood] = useState(foodsData)
+  const [search, setSearch] = useState('')
+
+  const handleDelete = (name) => {
+    const newFoods = foods.filter(food => food.name !== name)
+    setFood(newFoods)
+  }
+
+  const handleSearch = () => {}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>
+        <Search searching={setSearch}  />
+      </div>
+
+      <div>
+        <AddFoodForm foods={foods} />
+      </div>
+
+      <div>
+        <Divider> Food List </Divider>
+        {foods.map((food, index) => (
+          <Row key={index} >
+            <Col >
+              <div  >
+                <FoodBox 
+                food={{name: food.name, calories: food.calories, image: food.image, servings: food.servings}} 
+                foods={foods} 
+                handleDelete = {handleDelete}
+                />
+              </div>
+            </Col>
+          </Row>
+        ))}
+      </div>
+
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/addfoodform.jsx
@@ -0,0 +1,56 @@
+import { Divider, Input } from 'antd';
+import {useState} from 'react'
+
+function AddFoodForm(props) {
+
+  const [show, setShow] = useState(true)
+
+  return (
+    <div>
+      { show ? 
+        <form>
+          <Divider>Add Food Entry</Divider>
+
+          <label>Name</label>
+          <Input 
+            value={undefined} 
+            type="text" 
+            onChange={() => {}} 
+          />
+
+          <label>Image</label>
+          <Input 
+            value={undefined} 
+            type="text" 
+            onChange={() => {}} 
+          />
+
+          <label>Calories</label>
+          <Input 
+            value={undefined} 
+            type="number" 
+            onChange={() => {}} 
+          />
+
+          <label>Servings</label>
+          <Input 
+            value={undefined} 
+            type="number" 
+            onChange={() => {}} 
+          />
+
+          <button type="submit" onClick={() => {
+          }}>Create</button>
+
+          <button onClick={() => setShow(false)}>Hide Form</button>
+        </form> : null}
+
+
+
+    </div>
+
+
+  );
+}
+
+export default AddFoodForm;
